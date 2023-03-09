62bca9837c11a2de9e437d810c5604499b955dba
Abdulrahman <abdulrahmanheiba253@gmail.com>
2023-03-09 16:23:45 +0200

    latest

--- register.php
+++ register.php
@@ -1,43 +1,42 @@
 <?php
 
 require 'connection.php';
-$fullname=$_POST['fullname'];
-$email=$_POST['email'];
-$password=md5($_POST['password']);
-$university=$_POST['university'];
-$faculty=$_POST['faculty'];
-$image=$_POST['image'];
+$json_data = file_get_contents("php://input");
+$data = json_decode($json_data, true);
 
-
+$fullname=$data['fullname'];
+$email=$data['email'];
+$password=md5($data['password']);
+$university = $data['university'] ;
+$faculty=$data['faculty'];
+//$image= $_POST['image'];
 
 $checkUser="SELECT * from user WHERE email='$email'";
 $checkQuery=mysqli_query($con,$checkUser);
 
 if(mysqli_num_rows($checkQuery)>0){
-
-     $response['error']="403";
+     
+    while($row=$checkQuery->fetch_assoc())
+    $response['user']=$row;
     $response['message']="User exist";
 }
 else
 {
-   $insertQuery="INSERT INTO user(fullname,email,password,university,faculty,image) VALUES('$fullname','$email','$password','$university','$faculty','$image')";
-$result=mysqli_query($con,$insertQuery);
+   $insertQuery="INSERT INTO user(fullname,email,Password,university,faculty_id) VALUES('$fullname','$email','$password','$university','$faculty')";
+   $result=mysqli_query($con,$insertQuery);
 
-if($result){
-
-    $response['error']="200";
+if($insertQuery){
+    
+    $response['message'] = "";
     $response['message']="Register successful!";
-}
+ }
 else
 {
     $response['error']="400";
     $response['message']="Registeration failed!";
 }
 
-
-
 }
-
 
 echo json_encode($response);
 
