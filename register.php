5a624a79c1f5b4b2690b4a454b783c6d34a3ba2e
Abdulrahman <abdulrahmanheiba253@gmail.com>
2023-03-13 17:30:21 +0200

    latest

--- register.php
+++ register.php
@@ -17,21 +17,30 @@ $checkUser="SELECT * from user WHERE email='$email'";
 $checkQuery=mysqli_query($con,$checkUser);
 
 if(mysqli_num_rows($checkQuery)>0){
-    http_response_code(201);
-    $user = mysqli_fetch_assoc($checkQuery);
-    echo json_encode($user);
+
+
+    while($row=$checkQuery->fetch_assoc())
+    $response['user'] = $row;
+    $response['message'] = "user exist";
+    echo json_encode($response);
+    
 }
+
+
 else
 {
    $insertQuery="INSERT INTO user(fullname,email,Password,university,faculty_id,image) VALUES('$fullname','$email','$password','$university','$faculty_id','$image')";
    $result=mysqli_query($con, $insertQuery);
    if($result) {
-    http_response_code(201);
     $userId = mysqli_insert_id($con);
     $selectQuery = "SELECT * FROM user WHERE student_id = '$userId'";
     $userResult = mysqli_query($con, $selectQuery);
+    
     $user = mysqli_fetch_assoc($userResult);
-    echo json_encode($user);
+    $user['Password'] = '';
+    $response['user'] = $user;
+    $response['message'] = "Registered successfully";
+    echo json_encode($response);
 } else {    
     http_response_code(500);
     $response['message'] = "Failed to add the user";
