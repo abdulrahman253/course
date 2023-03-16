1a2948c889fddea8b830107c804971c86d45fe9d
Abdulrahman <abdulrahmanheiba253@gmail.com>
2023-03-16 19:30:51 +0200

    ..

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 
-   $hostName='localhost';
-   $userName='root';
-   $userPass='';
+   $hostName='database-1.cov72iohk5ci.us-east-1.rds.amazonaws.com';
+   $userName='admin';
+   $userPass='123456789';
    $dbName='test';
 
    $con=mysqli_connect($hostName,$userName,$userPass,$dbName);
